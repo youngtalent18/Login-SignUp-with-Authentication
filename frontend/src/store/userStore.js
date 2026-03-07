732069000e7ed1c275b78485634913e651dfc8f6
Stephen Anti <stephenanti63@gmail.com>
2026-03-07 15:22:31 -0800

    1-used zustand to fetch api instead of direct fetching in homepage

---
+++ frontend/src/store/userStore.js
@@ -0,0 +1,55 @@
+import {create} from "zustand"
+import {toast} from "react-hot-toast"
+import axios from "../api/axios.js"
+
+export default create((set)=>({
+    user: null,
+    checkingAuth: true,
+    loading: false,
+
+    logout: async () =>{
+        try {
+            await axios.post("/auth/logout");
+            set({user: null});
+        } catch (error) {
+            const message = error.message || "logout failed";
+            toast.error(message);
+        }
+    },
+    login: async ({username, password}) => {
+        set({loading: true});
+        try {
+            await axios.post("/auth/login",{username, password});
+            const res = await axios.get("/auth/me");
+            set({user: res.data, loading: false});
+        } catch (error) {
+            const message = error.response.data.error || "An error occurred";
+            toast.error(message);
+        }
+    },
+    signup: async ({name, email, password, confirmPassword}) => {
+        set({laoding: true});
+
+        if(password !== confirmPassword){
+            set({ loading: false});
+            return toast.error("Password mismatch");
+        }
+        try {
+            const res = await axios.post("/auth/signup",{name, email, password});
+            set({user: res.data, loading: false});
+        } catch (error) {
+            set({loading: false});
+            const message = error.response.data.error || "An error occurred";
+            toast.error(message);
+        }
+    },
+    checkAuth: async () => {
+        set({checkingAuth: true});
+        try {
+            const res = await axios.get("/auth/me");
+            set({user: res.data, checkingAuth: false});
+        } catch (error) {
+            set({checkingAuth: false, user: null});
+        }
+    },
+}));
